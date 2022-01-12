932feb415e12187b2e1b68a1a4c3b6d365086ac5
souvik1000 <souvik.ghosh.fiem.cse17@teamfuture.in>
2022-01-12 21:00:45 +0530

    working condition

--- docsy/receptionist/views.py
+++ docsy/receptionist/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import doctor
 
-# Create your views here.
+def login(request):
+    return render(request,'login.html')
+
+def registrationValidation(request):
+    name = request.POST['name']
+    specality = request.POST['specality']
+    gender = request.POST['gender']
+    phone = request.POST['phone']
+    password = request.POST['password']
+    clinic_address = request.POST['clinic_address']
+    submit_details=doctor(name=name,specality=specality,gender=gender,phone=phone,password=password,clinic_address=clinic_address)
+    submit_details.save()
+    return HttpResponse("Registration Successful")
+
+def loginauth(request):
+    phone=request.POST['a']
+    passw=request.POST['b']
+    doctors_table=doctor.objects.all()
+    for i in doctors_table:
+        if(i.phone==phone and i.password==passw):
+            return  HttpResponse(1)
+    return HttpResponse(0)
+
+def doctorsDashboard(request):
+    return HttpResponse("Doctors Dashboard")
