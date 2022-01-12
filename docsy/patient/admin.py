0a74ec716cc76d9774e43ebf54e0ed7dc0195c51
souvik1000 <souvik.ghosh.fiem.cse17@teamfuture.in>
2022-01-12 20:00:21 +0530

    provide admin access

--- docsy/patient/admin.py
+++ docsy/patient/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import patient
+from .models import allergies
 # Register your models here.
+admin.site.register(patient)
+admin.site.register(allergies)
--- docsy/receptionist/admin.py
+++ docsy/receptionist/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import doctor
+from .models import prescription
+from .models import problem
+from .models import medicines
+from .models import diagnostic
+from .models import labreport
+from .models import imagingexam
 # Register your models here.
+admin.site.register(doctor)
+admin.site.register(prescription)
+admin.site.register(problem)
+admin.site.register(medicines)
+admin.site.register(diagnostic)
+admin.site.register(labreport)
+admin.site.register(imagingexam)
