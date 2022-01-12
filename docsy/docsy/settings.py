4d27caf23355082940d9a7a4a989d0f071c31563
souvik1000 <souvik.ghosh.fiem.cse17@teamfuture.in>
2022-01-12 20:33:10 +0530

    setting changed

--- docsy/docsy/settings.py
+++ docsy/docsy/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'receptionist.apps.ReceptionistConfig',
+    'patient.apps.PatientConfig'
 ]
 
 MIDDLEWARE = [
