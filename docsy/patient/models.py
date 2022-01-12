192f4c076bfc0e41e688f1cf65661278ffc9e7d8
souvik1000 <souvik.ghosh.fiem.cse17@teamfuture.in>
2022-01-12 19:55:54 +0530

    Update Database

--- docsy/patient/models.py
+++ docsy/patient/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from datetime import datetime,date
 # Create your models here.
+
+class patient(models.Model):
+    name = models.TextField(max_length=100)
+    dob = models.DateField(default=date.today)
+    gender = models.TextField()
+    phoneno = models.TextField()
+    email=models.EmailField(unique=True,max_length=200)
+    address = models.TextField()
+    registration_time = models.DateField(default=date.today)
+    
+class allergies(models.Model):
+    patientId = models.ForeignKey(patient,on_delete=models.CASCADE)
+    substance = models.TextField()
+    criticality = models.TextField()
+    type = models.TextField()
+    comment = models.TextField()
+
+class procedurehistory(models.Model):
+    patientId = models.ForeignKey(patient,on_delete=models.CASCADE)
+    procedure_name = models.TextField()
+    body_site = models.TextField()
+    procedure_date = models.DateField(default=date.today)
+    
+class illnesshistory(models.Model):
+    patientId = models.ForeignKey(patient,on_delete=models.CASCADE)
+    illness_name = models.TextField()
+    body_site = models.TextField()
+    severity = models.TextField()
+    illness_date_onset = models.DateTimeField(default = datetime.now)
+    illness_date_abatement = models.DateTimeField(default = datetime.now)
+ 
--- docsy/receptionist/models.py
+++ docsy/receptionist/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from datetime import date, datetime
+from patient.models import patient
 
-# Create your models here.
+class doctor(models.Model):
+    name = models.TextField(max_length=100)
+    specalist = models.TextField(max_length=100, null=True)
+    password = models.TextField(max_length=1000)
+    email=models.EmailField(unique=True,max_length=200)
+    registration_time = models.DateField(default=date.today)
+
+
+class prescription(models.Model):
+    patientId = models.ForeignKey(patient,on_delete=models.CASCADE)
+    doctorId = models.ForeignKey(doctor,on_delete=models.CASCADE)
+    comment = models.TextField() 
+
+
+class problem(models.Model):
+    prescriptionId = models.ForeignKey(prescription,on_delete=models.CASCADE)
+    patientId = models.ForeignKey(patient,on_delete=models.CASCADE)
+    problem_name = models.TextField() 
+    problem_body_site = models.TextField() 
+    severity = models.TextField()
+    date_time_onset = models.DateTimeField(default=datetime.now)
+
+
+class medicines(models.Model):
+    prescriptionId = models.ForeignKey(prescription,on_delete=models.CASCADE)
+    medicine_name = models.TextField()
+    form = models.TextField()
+    strength = models.IntegerField()
+    strength_unit = models.TextField()
+    diluent = models.TextField()
+    diluent_amount = models.IntegerField()
+    diluent_unit = models.TextField()
+    dosade_directions = models.TextField()
+    frequency = models.IntegerField()
+    frequency_unit = models.TextField()
+    interval = models.IntegerField()
+    interval_unit = models.TextField()
+    named_time_event = models.TextField()
+    exact_timing_critical = models.BooleanField()
+    
+    
+class diagnostic(models.Model):
+    patientId = models.ForeignKey(patient,on_delete=models.CASCADE)
+    doctorId = models.ForeignKey(doctor,on_delete=models.CASCADE)
+    
+    
+class labreport(models.Model):
+    diagnosticId = models.ForeignKey(diagnostic,on_delete=models.CASCADE)
+    lab_event = models.TextField()
+    lab_test_name = models.TextField()
+    lab_specimen_type = models.TextField()
+    lab_specimen_method = models.TextField()
+    lab_specimen_body_site = models.TextField()
+    lab_findings = models.TextField()
+    lab_document = models.FileField(upload_to='images/', null=True, verbose_name="")
+
+
+class imagingexam(models.Model):
+    diagnosticId = models.ForeignKey(diagnostic,on_delete=models.CASCADE)
+    imaging_event = models.TextField()
+    imaging_test_name = models.TextField()
+    imaging_modality = models.TextField()
+    imaging_body_site = models.TextField()
+    imaging_findings = models.TextField()
+    imaging_document = models.FileField(upload_to='images/', null=True, verbose_name="")
+    
