dc11eafb9ac38c9280b907e558e7f10f5128d155
kunnathu pavan kumar <kunnathu.kumar@innovaccer.com>
2022-01-13 12:46:45 +0530

    dashboard added

--- docsy/receptionist/urls.py
+++ docsy/receptionist/urls.py
@@ -6,15 +6,8 @@ urlpatterns=[
     path('registrationValidation/',views.registrationValidation,name='registrationValidation'),
     path('loginauth/',views.loginauth,name="loginauth"),
     path('doctorsDashboard/',views.doctorsDashboard,name="doctorsDashboard"),
-<<<<<<< HEAD
     path('prescription/',views.doctorprescription,name="doctorprescription"),
     path('prescriptionBackend/',views.prescriptionBackend,name="prescriptionBackend"),
     path('logout/',views.logout,name="logout")
 
-=======
-    path('patientSummary/',views.patientsummary,name="patientsummary"),
-    path('patientSummary/allergies',views.allergies,name="allergies"),
-    path('patientSummary/historyofillness', views.historyofillness, name="historyofillness"),
-    path('patientSummary/patientIllnessCreation', views.patientIllnessCreation, name="patientIllnessCreation")
->>>>>>> 0b1e69d824fe2b10dcc90d6c560c700dfc632c85
 ]
