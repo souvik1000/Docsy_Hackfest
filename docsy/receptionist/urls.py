60a5d6c3877d1c32d935de3c777300fdb48ac5ff
souvik1000 <souvik.ghosh.fiem.cse17@teamfuture.in>
2022-01-13 22:42:46 +0530

    Lab Report Added

--- docsy/receptionist/urls.py
+++ docsy/receptionist/urls.py
@@ -7,22 +7,18 @@ urlpatterns=[
     path('registrationValidation/',views.registrationValidation,name='registrationValidation'),
     path('loginauth/',views.loginauth,name="loginauth"),
     path('doctorsDashboard/',views.doctorsDashboard,name="doctorsDashboard"),
-
-    path('patientSummary/procedure',views.procedure,name="procedure"),
-    path('procedurecreation/',views.procedurecreation,name="procedurecreation"),
-    
-    # path('patientSummary/',views.patientsummary,name="patientsummary"),
-    # path('patientSummary/allergies',views.allergies,name="allergies"),
-    # path('patientSummary/historyofillness',views.historyofillness,name="historyofillness"),
     path('prescription/',views.doctorprescription,name="doctorprescription"),
     path('prescriptionBackend/',views.prescriptionBackend,name="prescriptionBackend"),
     
+    
+    path('patientSummary/procedure',views.procedure,name="procedure"),
+    path('procedurecreation/',views.procedurecreation,name="procedurecreation"),
     path('patientSummary/', views.patientSummary, name="patientSummary"),
-    path('createPartionData/', views.createPartionData, name="createPartionData"),
+    path('createPatientData/', views.createPatientData, name="createPatientData"),
     path('patientIllnessCreation/', views.patientIllnessCreation, name="patientIllnessCreation"),
     path('patientAllergiesCreation/', views.patientAllergiesCreation, name="patientAllergiesCreation"),
-    path('historyofillness/', views.historyofillness, name="historyofillness"),
     path('patientSummaryView/', views.patientSummaryView, name="patientSummaryView"),
+    path('diaganosisReportCreation', views.diaganosisReportCreation, name="diaganosisReportCreation"),
     # path('patientIllnessView/', views.patientIllnessView, name="patientIllnessView"),
     # path('allergyview/', views.allergyview, name="allergyview"),
     path('logout/',views.logout,name="logout"),
