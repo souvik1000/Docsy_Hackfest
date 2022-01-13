ab83b950bb88c73e4af96c6218a48b85444644ba
kunnathu pavan kumar <kunnathu.kumar@innovaccer.com>
2022-01-13 10:24:24 +0530

    presicription done

--- docsy/receptionist/urls.py
+++ docsy/receptionist/urls.py
@@ -5,5 +5,9 @@ urlpatterns=[
     path('',views.login,name="login"),
     path('registrationValidation/',views.registrationValidation,name='registrationValidation'),
     path('loginauth/',views.loginauth,name="loginauth"),
-    path('doctorsDashboard/',views.doctorsDashboard,name="doctorsDashboard")
+    path('doctorsDashboard/',views.doctorsDashboard,name="doctorsDashboard"),
+    path('prescription/',views.doctorprescription,name="doctorprescription"),
+    path('prescriptionBackend/',views.prescriptionBackend,name="prescriptionBackend"),
+    path('logout/',views.logout,name="logout")
+
 ]
