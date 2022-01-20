06014cf17b9412f52fcf1e04d840b461696f9de3
kirti garg <kirti.garg@innovaccer.com>
2022-01-20 14:14:20 +0530

    "download reports section testing done"

--- docsy/patient/tests.py
+++ docsy/patient/tests.py
@@ -1,3 +1,4 @@
+from unittest import expectedFailure
 from django.test import TestCase
 
 from distutils.core import setup
@@ -7,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # Create your tests here.
-class PlayerFormTest(LiveServerTestCase):
+class PatientFormTest(LiveServerTestCase):
     selenium = webdriver.Chrome()
     
     def test4_patient_download_report(self):
@@ -21,8 +22,30 @@ class PlayerFormTest(LiveServerTestCase):
         time.sleep(4) 
         driver.maximize_window()
         driver.execute_script('window.scrollTo(0,450);') 
-        driver.sleep(4)
+        time.sleep(4)
         clickreports=driver.find_element_by_xpath('/html/body/div/section[2]/div/div/div[2]/a').click()
+        time.sleep(5)
+        downloadlab=driver.find_element_by_xpath('/html/body/div/section[3]/div/div[2]/div[1]/div/div[4]/div/div/div/a')
+        time.sleep(2)
+        driver.execute_script("arguments[0].click();",downloadlab)
+        time.sleep(3)
+        checkdownloadactual=driver.find_element_by_xpath('/html/body/div[2]/section/div/div[2]/div/div/div/div/div[1]/div[1]/p/b').text
+        expected='Name :'
+        self.assertEquals(checkdownloadactual,expected)
+        time.sleep(3)
+        driver.get('http://127.0.0.1:8000/patient/')
+        driver.maximize_window()
+        driver.execute_script('window.scrollTo(0,450);') 
+        time.sleep(4)
+        clickreports2=driver.find_element_by_xpath('/html/body/div/section[2]/div/div/div[3]/a').click()
+        time.sleep(5)
+        downloadlab2=driver.find_element_by_xpath('/html/body/div/section[4]/div/div[2]/div[1]/div/div[4]/div/div/div/a')
+        time.sleep(2)
+        driver.execute_script("arguments[0].click();",downloadlab2)
+        time.sleep(3)
+        checkdownloadactual2=driver.find_element_by_xpath('/html/body/div[2]/section/div/div[2]/div/div/div/div/div[1]/div[1]/p/b').text
+        expected2='Name :'
+        self.assertEquals(checkdownloadactual2,expected2)
 
 # # Create your tests here.
 # class PlayerFormTest(LiveServerTestCase):
