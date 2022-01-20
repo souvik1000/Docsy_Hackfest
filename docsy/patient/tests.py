7844136666afedad10e5932c8f0d0522f18f15dd
kirti garg <kirti.garg@innovaccer.com>
2022-01-20 14:13:46 +0530

    "download reports section testing done"

--- docsy/patient/tests.py
+++ docsy/patient/tests.py
@@ -1,10 +1,28 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# from distutils.core import setup
-# from django.test import LiveServerTestCase
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# import time
+from distutils.core import setup
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Create your tests here.
+class PlayerFormTest(LiveServerTestCase):
+    selenium = webdriver.Chrome()
+    
+    def test4_patient_download_report(self):
+        driver = self.selenium
+        driver.get('http://127.0.0.1:8000/patient/patientlogin/')
+        email=driver.find_element_by_xpath('//*[@id="sign_in_email"]')
+        email.send_keys('patient@innovaccer.com')
+        password2=driver.find_element_by_xpath('//*[@id="sign_in_pass"]')
+        password2.send_keys('A12@asdfgh')
+        login_submit=driver.find_element_by_xpath('//*[@id="container"]/div[2]/form/button').click()
+        time.sleep(4) 
+        driver.maximize_window()
+        driver.execute_script('window.scrollTo(0,450);') 
+        driver.sleep(4)
+        clickreports=driver.find_element_by_xpath('/html/body/div/section[2]/div/div/div[2]/a').click()
 
 # # Create your tests here.
 # class PlayerFormTest(LiveServerTestCase):
