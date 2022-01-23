4363e92deea4065fba9c66054704c8bdc6cf86d0
Raghib shams <raghibshams456@gmail.com>
2022-01-23 18:47:53 +0530

    Update tests.py

--- docsy/receptionist/tests.py
+++ docsy/receptionist/tests.py
@@ -15,12 +15,15 @@ import time
 
 # Create your tests here.
 class ReceptionistAppTest(LiveServerTestCase):
-#     chrome_options = Options()
-#     chrome_options.add_argument('--headless')
-#     chrome_options.add_argument('--no-sandbox')
-#     chrome_options.add_argument('--disable-dev-shm-usage')
-    selenium = webdriver.Chrome()
-    
+    chrome_options = Options()
+
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    selenium = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+
+
+
     def test_01_patient_details_allergies(self):
         driver = self.selenium 
         driver.maximize_window()
