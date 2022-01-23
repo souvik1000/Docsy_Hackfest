094f56ce871db0d9530811895bdc5aa63bbc17f1
souvik1000 <souvik.ghosh.fiem.cse17@teamfuture.in>
2022-01-24 03:59:44 +0530

    database changed

--- docsy/docsy/settings.py
+++ docsy/docsy/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'docsy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-# 'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': 'postgres',
-#     'USER': 'postgres',
-#     'PASSWORD': 'postgres',
-#     'HOST': 'docsydatabase.cayn3utpakrm.us-east-1.rds.amazonaws.com',
-#     'PORT': '5432',
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'postgres',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': 'docsydatabase.cayn3utpakrm.us-east-1.rds.amazonaws.com',
+    'PORT': '5432',
+    }
+}
 
 
 # Password validation
